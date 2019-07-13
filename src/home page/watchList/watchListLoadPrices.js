75b7920c1bca5454153a3455c52bb376cc144b4a
osamahan999 <hanhan.osama@gmail.com>
2019-07-13 15:35:22 -0700

    made watchListLoadPrices.js better. fixed wsbRegister.php initializing fake stock

--- src/home page/watchList/watchListLoadPrices.js
+++ src/home page/watchList/watchListLoadPrices.js
@@ -1,5 +1,6 @@
 //this is my API key
-const keyToken = "api_token=Yo0mpIdLPhffFMpfbbn5QOfCIBsq5vnH3LdlnU0rbEKHH8D5ZlHtzTpz3lWe";
+const keyToken1 = "api_token=Yo0mpIdLPhffFMpfbbn5QOfCIBsq5vnH3LdlnU0rbEKHH8D5ZlHtzTpz3lWe";
+const keyToken2 = "api_token=2DnezxV4AGG6QUJvMhxs1B0UmTtdJOQ1Nzld9uGZCraYHpOUO7baA7AmB0av";
 //this is the home link
 const home = "https://intraday.worldtradingdata.com/api/v1/intraday?symbol=";
 
@@ -20,9 +21,12 @@ function setStocks(stocks) {
 function getWatchList(username) {
 	console.log("this got called!");
 	
+	//username of person we're loading the watchlist for
 	console.log(username);
 	
-	
+	/**
+	 * a get-ajax request which takes in a username and returns an array of strings. 
+	 */
 	$.ajax({
 		url: 'http://localhost/wsb/src/home%20page/watchList/watchListLoadPrices.php',
 		type: 'get',
@@ -31,9 +35,12 @@ function getWatchList(username) {
 		
 		success: function(result){
 			arr = result.watchList;
+			//sets array to be global
 			setArray(arr);
 			
+			//calls get prices which ideally should populate the stocks array with last 30 minutes of stock data for each watchlist stock
 			getPrices(arr);
+			// prints the array of stocks
 			printArray(stocks);
 			
 		},
@@ -45,17 +52,20 @@ function getWatchList(username) {
 	});
 }
 
+//takes in an array of strings. 
 function getPrices(watchList) {
+	//called for all stocks in watch list
 	for (var i = 0; i < watchList.length; i++) {
-		var Url = "https://intraday.worldtradingdata.com/api/v1/intraday?symbol=" + watchList[i] + "&range=1&interval=1&" + keyToken;			
-
+		var Url = "https://intraday.worldtradingdata.com/api/v1/intraday?symbol=" + watchList[i] + "&range=1&interval=1&" + keyToken2;			
+		
+		//if stock is not null, continue
 		if (watchList[i] != null) {
 			$.ajax({
 				url: Url,
 				type: "GET",
 				async: false,
 				success: function(result){
-					
+					//populates our stock array with data for said non-null stock
 					populateArray(result);
 				},
 				error: function(error){
@@ -66,28 +76,29 @@ function getPrices(watchList) {
 	}
 }
 
+//takes in array of stock data. creates an array of 30 mins of that data and stores it in stocks 
 function populateArray(result) {
 	
 	var counter = 0;
 	var newArray = new Array();
 	
-	newArray.push(result["symbol"]);
 	
 	for (var x in result["intraday"]) {
-		if (counter > 30) break;		
+		//only 30 mins worth of data
+		if (counter > 30) break;
+		
 		newArray.push(result["intraday"][x]["open"]);
 		counter++;
 	}
+	stocks[result["symbol"]] = newArray;
 	
-	stocks.push(newArray);
 	setStocks(stocks);
 }
 
 function printArray(stocks) {
-	console.log("print array got called");
 	
-	for (var i = 0; i < stocks.length; i++) {
-		console.log(i + ' ' + stocks[i][0]);
+	for (var x in stocks) {
+		console.log(x);
 	}
 	
 	
