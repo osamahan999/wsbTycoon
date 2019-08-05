cdc5bd8989bde063aa9093a3ebee45eea103a002
Matthew Kwong <10681480+MatthewKwong@users.noreply.github.com>
2019-08-05 14:42:14 -0700

    fix the stockPage.html form

--- src/stockPurchase/stockPurchase.js
+++ src/stockPurchase/stockPurchase.js
@@ -1,14 +1,9 @@
 
-
-
 function purchaseStock() {
-	document.write("purchase stock called");
-	
-	stock = document.getElementById("myStock"); 
-	amt = document.getElementById("amt");
-	
-	document.write (stock);
-	
-	
-}
 
+    stock = document.getElementById("myStock").value;
+    amt  = document.getElementById("amt").value;
+
+		//Writing submission to innerHTML
+    document.getElementById("output").innerHTML = "The stock is " + stock + " and the amount is " + amt ".";
+}
