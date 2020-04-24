7c20152b65129461295365553c6efebfd8282cbf
osamahan999 <hanhan.osama@gmail.com>
2020-04-23 21:20:02 -0700

    buyStock.php created.

---
+++ src/purchase page/buyStock.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+/**
+ * 
+ * check if stock amt, stock price, and stock total and username and datetime are set
+ * if so, create new transaction for user
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+?>
